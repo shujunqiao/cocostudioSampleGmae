7916c1302e1a8e9abc44c817f867f3fb41393d90
shujun.qiao <shujun.qiao@chukong-inc.com>
2013-10-16 10:13:24 +0800

    add tiled map.

--- cocos2d-html5/IronCity/src/scene/GameScene.js
+++ cocos2d-html5/IronCity/src/scene/GameScene.js
@@ -2,6 +2,10 @@ var GameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
 
+        var moveMap = new MovedMap();
+        moveMap.init();
+        this.addChild(moveMap, 0);
+
         var playLayer = new GameScenePlayLayer();
         playLayer.init();
         this.addChild(playLayer, 0);
