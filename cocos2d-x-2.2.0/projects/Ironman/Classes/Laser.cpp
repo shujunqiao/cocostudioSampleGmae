a6df8478c220588d816e3d66d477c9c6a371e8a0
Ken <callmelina@163.com>
2013-10-18 17:53:41 +0800

    add laser class and resources.

--- cocos2d-x-2.2.0/projects/Ironman/Classes/Laser.cpp
+++ cocos2d-x-2.2.0/projects/Ironman/Classes/Laser.cpp
@@ -7,3 +7,32 @@
 //
 
 #include "Laser.h"
+
+bool Laser::init(CCPoint position, float direction)
+{
+	this->create("iphone/laser.png");
+	this->setPosition(position);
+	this->setAnchorPoint(ccp(0, 0));
+	this->setRotation(direction);
+	
+	return true;
+}
+
+void Laser::releaseLaser()
+{
+	this->removeFromParentAndCleanup(true);
+}
+
+bool Laser::ifOutSideWall()
+{
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+	float laserX = this->getPosition().x;
+	float laserY = this->getPosition().y;
+	
+	if(laserX < 0.0f || laserX > winSize.width || laserY < 0.0f || laserY > winSize.height){
+		return true;
+	}
+	
+
+	return false;
+}
